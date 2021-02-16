232a1309080c198457bd8d6c975d81bbebab2c4f
LordLalwani <Deepplalwani@hotmail.com>
2021-02-16 23:14:56 +1300

    Refactor file

--- TitanMVP.ino
+++ TitanMVP.ino
@@ -2,48 +2,27 @@
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-#include "Wire.h"
-#endif
-
 MPU6050 mpu;
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
 
-// MPU control/status vars
-bool dmpReady = false; // set true if DMP init was successful
-uint8_t mpuIntStatus;  // holds actual interrupt status byte from MPU
-uint8_t devStatus; // return status after each device operation (0 = success, !0
-                   // = error)
-uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;     // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64]; // FIFO storage buffer
+Quaternion q;        // quaternion container [w, x, y, z]
+VectorInt16 aa;      // accel sensor measurements [x, y, z]
+VectorInt16 aaReal;  // gravity-free accel sensor measurements [x, y, z]
+VectorInt16 aaWorld; // world-frame accel sensor measurements [x, y, z]
+VectorFloat gravity; // gravity vector [x, y, z]
+float euler[3];      // Euler angle container [psi, theta, phi]
+float ypr[3];        // yaw/pitch/roll container and gravity vector [yaw, pitch, roll]
 
-// orientation/motion vars
-Quaternion q;   // [w, x, y, z]         quaternion container
-VectorInt16 aa; // [x, y, z]            accel sensor measurements
-VectorInt16
-    aaReal; // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16
-    aaWorld; // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity; // [x, y, z]            gravity vector
-float euler[3];      // [psi, theta, phi]    Euler angle container
-float
-    ypr[3]; // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-int highestY;
-int HighestGravity;
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt =
-    false; // indicates whether MPU interrupt pin has gone high
+volatile bool mpuInterrupt = false;
 void dmpDataReady() { mpuInterrupt = true; }
 
 void setup() {
   Serial.begin(115200);
-  highestY = 0;
   pinMode(8, OUTPUT);
 
 // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -55,8 +34,7 @@ void setup() {
 #endif
 
   mpu.initialize();
-  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful")
-                                      : F("MPU6050 connection failed"));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
   devStatus = mpu.dmpInitialize();
 
   // MPU6050 calibration
@@ -65,26 +43,19 @@ void setup() {
   mpu.setZGyroOffset(54);
   mpu.setXAccelOffset(-2773);
   mpu.setYAccelOffset(-1424);
-  mpu.setZAccelOffset(1250); // 1688 factory default for my test chip
+  mpu.setZAccelOffset(1250);
 
-  // make sure it worked (returns 0 if so)
+  // make sure DMP initialized (returns 0 if so)
   if (devStatus == 0) {
     mpu.setDMPEnabled(true);
-
     // enable Arduino interrupt detection
-    Serial.println(
-        F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
     attachInterrupt(0, dmpDataReady, RISING);
     mpuIntStatus = mpu.getIntStatus();
     dmpReady = true;
-
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
   } else {
-    // ERROR!
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-    // (if it's going to break, usually the code will be 1)
     Serial.print(F("DMP Initialization failed (code "));
     Serial.print(devStatus);
     Serial.println(F(")"));
@@ -94,29 +65,16 @@ void setup() {
 void loop() {
   if (!dmpReady)
     return;
-  while (!mpuInterrupt && fifoCount < packetSize) {
-    // other program behavior stuff here
-    // if you are really paranoid you can frequently test in between other
-    // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-    // while() loop to immediately process the MPU data
-  }
 
   // reset interrupt flag and get INT_STATUS byte
   mpuInterrupt = false;
   mpuIntStatus = mpu.getIntStatus();
-
-  // get current FIFO count
   fifoCount = mpu.getFIFOCount();
 
-  // check for overflow (this should never happen unless our code is too
-  // inefficient)
+  // check for overflow (this should never happen unless our code is too inefficient)
   if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
     mpu.resetFIFO();
     Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen
-    // frequently)
   } else if (mpuIntStatus & 0x02) {
     // wait for correct available data length, should be a VERY short wait
     while (fifoCount < packetSize)
@@ -129,20 +87,15 @@ void loop() {
     // (this lets us immediately read more without waiting for an interrupt)
     fifoCount -= packetSize;
 
-    // display real acceleration, adjusted to remove gravity
+    // calculations below:
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetAccel(&aa, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
     mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
 
-    while (aaWorld.y >= 0) {
-      if (highestY <= aaWorld.y) {
-        digitalWrite(8, HIGH);
-        highestY = aaWorld.y;
-        Serial.println(highestY);
-      }
-      digitalWrite(8, LOW);
+    if (aaWorld.y >= 0) {
+      Serial.println(aaWorld.y);
     }
   }
 }
